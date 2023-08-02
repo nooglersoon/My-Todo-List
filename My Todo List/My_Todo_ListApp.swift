be244b92b85ccd4ec3ae5633786a1bc8fe0c4e64
nooglersoon <nooglersoon@outlook.co.id>
2023-08-02 09:50:58 +0700

    Initial Commit

---
+++ My Todo List/My_Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Todo_ListApp.swift
+//  My Todo List
+//
+//  Created by Fauzi Achmad B D on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
