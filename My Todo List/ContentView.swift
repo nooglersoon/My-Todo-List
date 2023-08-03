3eaa4b312830c64344e9b21c2281ee2b4058582f
nooglersoon <nooglersoon@outlook.co.id>
2023-08-03 11:57:09 +0700

    [style]: Restructuring Views
    - In order to make the ContentView independent from its module, its better to wrap the list view into its own views
    - Create TodoList module to increase code readability + clean

--- My Todo List/ContentView.swift
+++ My Todo List/ContentView.swift
@@ -16,38 +16,11 @@ struct ContentView: View {
         self.viewModel = viewModel
     }
     
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Todo.dueDate,ascending: true)],
-        animation: .default
-    )
-    private var items: FetchedResults<Todo>
-    
     @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
-            List {
-                if items.count > 0 {
-                    // Get the todo items from the persistence store through viewModel
-                    ForEach(items) { todo in
-                        NavigationLink {
-                            FormView(viewModel: viewModel, todo: todo)
-                        } label: {
-                            TodoCellView(
-                                title: todo.title ?? "",
-                                dueDate: todo.dueDate,
-                                desc: todo.desc
-                            )
-                        }
-                    }
-                    .onDelete { indexSet in
-                        viewModel.deleteItems(items: items, offsets: indexSet)
-                    }
-                } else {
-                    // Empty States
-                    Text("No current todos")
-                }
-            }
+            TodoListView(viewModel: viewModel)
             .sheet(isPresented: $showingAlert, content: {
                 FormView(viewModel: viewModel)
             })
@@ -64,9 +37,3 @@ struct ContentView: View {
         }
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
---
+++ My Todo List/Modules/TodoList/TodoListView.swift
@@ -0,0 +1,54 @@
+//
+//  TodoListView.swift
+//  My Todo List
+//
+//  Created by Fauzi Achmad B D on 03/08/23.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    // Call the viewModel environment object
+    let viewModel: TodoViewModel
+    
+    init(viewModel: TodoViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Todo.dueDate,ascending: true)],
+        animation: .default
+    )
+    private var items: FetchedResults<Todo>
+    
+    var body: some View {
+        List {
+            if items.count > 0 {
+                // Get the todo items from the persistence store through viewModel
+                ForEach(items) { todo in
+                    NavigationLink {
+                        FormView(viewModel: viewModel, todo: todo)
+                    } label: {
+                        TodoCellView(
+                            title: todo.title ?? "",
+                            dueDate: todo.dueDate,
+                            desc: todo.desc
+                        )
+                    }
+                }
+                .onDelete { indexSet in
+                    viewModel.deleteItems(items: items, offsets: indexSet)
+                }
+            } else {
+                // Empty States
+                Text("No current todos")
+            }
+        }
+    }
+}
+
+//struct TodoListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TodoListView()
+//    }
+//}
